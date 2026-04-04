605fbb2f3d5051e0ae7ccaefc3463c6b1e875d9e
joao <jmatheusdeoliveira341@gmail.com>
2026-04-04 17:11:54 -0300

    finalizado funções principais do projeto

--- debug.js
+++ debug.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import fs from 'fs/promises' 
-import { Sequelize, Op, DataTypes } from 'sequelize';
+import { Sequelize, Op, DataTypes, JSONB } from 'sequelize';
 
 const sequelize = new Sequelize('testes_BD2', process.env.USER_DB, process.env.SENHA, {
   host: process.env.IP, 
@@ -29,7 +29,8 @@ const jobs = sequelize.define(
   }
 );
 
-people.hasMany(jobs,{foreingkey:'job_ID'});
+jobs.hasMany(people,{foreingkey:'job_ID'});
+people.belongsTo(jobs, {foreingkey:'job_ID'});
 
 await sequelize.sync({force:true});
 
@@ -52,7 +53,7 @@ async function iniciarMigracao() {
         sex : colunas[4],
         email : colunas[5],
         phone : colunas[6],
-        dateOfBrith : colunas[7],
+        dateOfBirth : colunas[7],
         jobTitle : colunas[8]
       };
     }).filter(p => p.nome); 
@@ -64,11 +65,35 @@ async function iniciarMigracao() {
 
 let inputs = await iniciarMigracao();
 
-async function insereJob(jobNome){
-    jobs.create({
-      jobTitle: jobNome
+async function insereJob(jobNome){   
+    let result = await jobs.findAll({
+        attributes: ['jobTitle','jobID']
+    });
+    
+    for( let i = 0; i < result.length; i++){
+        if (jobNome === result[i].dataValues.jobTitle){
+            return result[i].dataValues.jobID;
+        }
+    }
+    let newJob = await jobs.create({
+    jobTitle: jobNome
     })
+    return newJob.dataValues.jobID;
 };
+
+async function inserePeople(pessoa){
+    await people.create({
+        index: pessoa.index,
+        userID: pessoa.id,
+        firstName: pessoa.nome,
+        lastName: pessoa.sobrenome,
+        sex: pessoa.sex,
+        email : pessoa.email,
+        phone : pessoa.phone,
+        dateOfBirth: pessoa.dateOfBirth,
+        jobID: pessoa.jobTitle
+    })
+}
 
 function mostraTabela(tabela){
     const tabularData = tabela.map(result => result.get({plain:true}));
@@ -79,31 +104,30 @@ async function mostraJobs(){
   let result = await jobs.findAll();
   mostraTabela(result);
 }
-async function retornaAddress(){
-    const result =  await Address.findAll(
-        {include: {
-            model: City,
-            include: {
-                model: Country
-            }
-        }}
-    );
-    mostraTabela(result);
-};
+
+async function mostraPeople(){
+  let result = await people.findAll();
+  mostraTabela(result);
+}
+
 
 async function populaTabela(tabela){
     for( let i = 0; i < tabela.length; i++){
         tabela[i].index = tabela[i].index.replace(/"/g,'');
         tabela[i].index = parseInt(tabela[i].index);
-        tabela[i].jobTitle = tabela[i].jobTitle.replace(/""/g,'');
-        await insereJob(tabela[i].jobTitle);
-        tabela[i].jobTitle = i;
-        console.log(tabela[i]);
-        break;
+        tabela[i].jobTitle = tabela[i].jobTitle.replace(/"/g,'');
+        tabela[i].jobTitle = tabela[i].jobTitle.replace(/\r/g,'');
+        let title = await insereJob(tabela[i].jobTitle);
+        tabela[i].jobTitle = title;
+        console.log(tabela[i].dateOfBirth);
+        inserePeople(tabela[i]);
     }
-    mostraJobs();
 }
 
 
 
-populaTabela(inputs);
+await populaTabela(inputs);
+
+mostraPeople();
+
+mostraJobs();
